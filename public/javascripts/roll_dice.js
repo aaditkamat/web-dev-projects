88db77e61f927ded446cc6b5d249e5870f7dab99
Aadit Kamat <unknown>
2022-05-15 09:20:43 +0800

    style: fix formatting and code style issues

--- public/javascripts/roll_dice.js
+++ public/javascripts/roll_dice.js
@@ -1,21 +1,19 @@
-var updatePage = () => {
-    let heading = document.getElementsByTagName('h1')[0];
-    let images = document.getElementsByTagName('img');
-    let getRandomInt = (min, max) => {
-        return Math.floor(Math.random() * max) + min;
-    } 
-    let scores = [];
-    for (let i = 0; i < images.length; i++) {
-        scores[i] = getRandomInt(1, 6);
-        images[i].src = `images/dice${scores[i]}.png`;
-    }
-    if (scores[0] > scores[1]) {
-        heading.innerText = '🚩 Player 1 wins!';
-    } else if (scores[1] > scores[0]) {
-        heading.innerText = 'Player 2 wins! 🚩';
-    } else {
-        heading.innerText = 'Draw!';
-    }
-}
+const updatePage = () => {
+  const heading = document.getElementsByTagName('h1')[0];
+  const images = document.getElementsByTagName('img');
+  const getRandomInt = (min, max) => Math.floor(Math.random() * max) + min;
+  const scores = [];
+  for (let i = 0; i < images.length; i += 1) {
+    scores[i] = getRandomInt(1, 6);
+    images[i].src = `images/dice${scores[i]}.png`;
+  }
+  if (scores[0] > scores[1]) {
+    heading.innerText = '🚩 Player 1 wins!';
+  } else if (scores[1] > scores[0]) {
+    heading.innerText = 'Player 2 wins! 🚩';
+  } else {
+    heading.innerText = 'Draw!';
+  }
+};
 
 updatePage();
