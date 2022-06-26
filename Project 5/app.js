02df85d380f086621623632c0d8447221991b57b
Aadit Kamat <unknown>
2022-06-26 21:27:24 +0800

    Add routes for single article

--- Project 5/app.js
+++ Project 5/app.js
@@ -4,8 +4,8 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/wikiDB");
 
 const articleSchema = new mongoose.Schema({
-    title: "string",
-    content: "string"
+  title: "string",
+  content: "string",
 });
 
 const Article = new mongoose.model("article", articleSchema);
@@ -25,49 +25,102 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.route("/articles")
-    .get((req, res) => {
-        Article.find((err, docs) => {
-            if (err) {
-                res.send(err);
-            } else {
-                res.send(docs);
-            }
-        });
-    })
-    .post((req, res) => {
-        const article = new Article({
-            title: req.body.title,
-            content: req.body.content
-        });
-        article.save()
-        .then(() => res.send("Article created successfully"))
-        .catch((err) => res.send(err));
-    })
-    .delete((req, res) => {
-        Article.deleteMany((err) => {
-            if (err) {
-                res.send(err);
-            } else {
-                res.send("All articles deleted successfully");
-            }
-        });
+app
+  .route("/articles")
+  .get((req, res) => {
+    Article.find((err, articles) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(articles);
+      }
     });
+  })
+  .post((req, res) => {
+    const article = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
+    article
+      .save()
+      .then(() => res.send("Article created successfully"))
+      .catch((err) => res.send(err));
+  })
+  .delete((req, res) => {
+    Article.deleteMany((err) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send("All articles deleted successfully");
+      }
+    });
+  });
 
-    // catch 404 error and forward to error handler
+app
+  .route("/articles/:articleTitle")
+  .get((req, res) => {
+    const title = req.params.articleTitle.split("-").join(" ");
+    Article.findOne({ title: new RegExp(title, "i") }, (err, article) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(article);
+      }
+    });
+  })
+  .put((req, res) => {
+    const title = req.params.articleTitle.split("-").join(" ");
+    Article.updateOne(
+      { title: new RegExp(title, "i") },
+      { title: req.body.title, content: req.body.content },
+      (err) => {
+        if (err) {
+          res.send(err);
+        } else {
+          res.send("Article updated successfully");
+        }
+      }
+    );
+  })
+  .patch((req, res) => {
+    const title = req.params.articleTitle.split("-").join(" ");
+    Article.update(
+      { title: new RegExp(title, "i") },
+      { $set: req.body },
+      (err) => {
+        if (err) {
+          res.send(err);
+        } else {
+          res.send("Article updated successfully");
+        }
+      }
+    );
+  })
+  .delete((req, res) => {
+    const title = req.params.articleTitle.split("-").join(" ");
+    Article.deleteOne({ title: new RegExp(title, "i") }, (err) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send("Article deleted successfully");
+      }
+    });
+  });
+
+// catch 404 error and forward to error handler
 app.use((req, res, next) => {
-    next(createError(404));
+  next(createError(404));
 });
-  
-  // error handler
+
+// error handler
 app.use((err, req, res) => {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
-  
-    // render the error page
-    res.status(err.status || 500);
-    res.render("error");
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 app.listen(port, () => {
